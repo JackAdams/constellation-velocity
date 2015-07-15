88c2b72967bf2cfc2d80b0e21d131acd525989a1
Brent Abrahams <brent_abrahams@yahoo.com>
2015-07-15 22:11:10 +0200

    Updated dependency

--- package.js
+++ package.js
@@ -1,18 +1,19 @@
 Package.describe({
   name: 'constellation:velocity',
   summary: 'Reactive Velocity test reports in Constellation',
-  version: '0.1.6',
+  version: '0.1.7',
+  documentation: 'README.md',
   git: 'https://github.com/JackAdams/constellation-velocity.git',
   debugOnly: true
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.0');
+  api.versionsFrom('1.1');
   api.use('velocity:core@0.6.3', 'client');
 
   api.use(['underscore', 'templating','amplify@1.0.0', 'less', 'tracker', 'session'], 'client');
   
-  api.use('constellation:console@1.0.6');
+  api.use('constellation:console@1.0.7');
 
   api.addFiles('lib/reamplify.js', 'client');
 
@@ -21,4 +22,5 @@ Package.onUse(function(api) {
   api.addFiles('lib/client-report.js', 'client');
   api.addFiles('lib/client-report.less', 'client');
   
+  api.imply('constellation:console');
 });
