869ac3b4cb0ac4b50c0c51c0ba7ccf0aceb46703
Brent Abrahams <brent_abrahams@yahoo.com>
2015-07-17 14:11:59 +0200

    Updated dependencies

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'constellation:velocity',
   summary: 'Reactive Velocity test reports in Constellation',
-  version: '0.1.7',
+  version: '0.2.0',
   documentation: 'README.md',
   git: 'https://github.com/JackAdams/constellation-velocity.git',
   debugOnly: true
@@ -13,7 +13,7 @@ Package.onUse(function(api) {
 
   api.use(['underscore', 'templating','amplify@1.0.0', 'less', 'tracker', 'session'], 'client');
   
-  api.use('constellation:console@1.0.7');
+  api.use('constellation:console@1.1.0');
 
   api.addFiles('lib/reamplify.js', 'client');
 
