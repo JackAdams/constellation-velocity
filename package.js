c4373137dcef9fe58a86087b5bcaa276e216247a
Brent Abrahams <brent_abrahams@yahoo.com>
2015-10-15 00:36:25 +0800

    Updated dependencies

--- package.js
+++ package.js
@@ -1,22 +1,23 @@
 Package.describe({
   name: 'constellation:velocity',
   summary: 'Reactive Velocity test reports in Constellation',
-  version: '0.3.1',
+  version: '0.3.5',
   documentation: 'README.md',
   git: 'https://github.com/JackAdams/constellation-velocity.git',
   debugOnly: true
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1');
-  api.use('velocity:core@0.6.3', 'client');
+  api.versionsFrom('1.2');
+  api.use('velocity:core@0.10.0', 'client');
 
-  api.use(['underscore', 'templating','amplify@1.0.0', 'less', 'tracker', 'session'], 'client');
+  api.use(['underscore', 'templating','amplify@1.0.0', 'less@2.5.0_3', 'tracker', 'session'], 'client');
   
-  api.use('constellation:console@1.2.1');
+  api.use('constellation:console@1.2.2');
 
   api.addFiles('lib/reamplify.js', 'client');
 
+  api.addFiles('lib/velocity.less', 'client');
   api.addFiles('lib/velocity.js', 'client');
   api.addFiles('lib/client-report.html', 'client');
   api.addFiles('lib/client-report.js', 'client');
